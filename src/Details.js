534116e78851a55d7b04e6ae897df8f69933c8b5
David Keathley <keathleydavidj@gmail.com>
2022-06-09 16:30:58 -0700

    replace class constructor with class properties

--- src/Details.js
+++ src/Details.js
@@ -2,17 +2,14 @@ import { Component } from "react";
 import { useParams } from "react-router-dom";
 
 class Details extends Component {
-  constructor() {
-    super();
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     );
     const json = await res.json();
-    this.setState(Object.assign({ loading: false }, json.pets[0]));
+    this.setState({ loading: false, ...json.pets[0] });
   }
 
   render() {
