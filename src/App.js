381eabf16d81d96ff39e94e81c42ef2408e175d1
David Keathley <keathleydavidj@gmail.com>
2022-06-13 12:35:38 -0700

    Using context for theming. Global state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { StrictMode, useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { StrictMode } from "react";
 import SearchParams from "./SearchParams.js";
 import Details from "./Details.js";
+import ThemeContext from "./ThemeContext.js";
 
 const App = () => {
+  const theme = useState("darkblue");
+
   return (
-    <StrictMode>
-      <BrowserRouter>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </BrowserRouter>
-    </StrictMode>
+    <ThemeContext.Provider value={theme}>
+      <StrictMode>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </StrictMode>
+    </ThemeContext.Provider>
   );
 };
 
