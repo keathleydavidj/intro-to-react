3f4084ccb27909e2b013dfbe562e148d392023e2
David Keathley <keathleydavidj@gmail.com>
2022-06-17 14:40:59 -0700

    lazy load the whole app, details route, and modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import SearchParams from "./SearchParams.js";
-import Details from "./Details.js";
 import ThemeContext from "./ThemeContext.js";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -11,15 +12,17 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <StrictMode>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
+        <Suspense fallback={<h2>loading, be patient my friend</h2>}>
+          <BrowserRouter>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
       </StrictMode>
     </ThemeContext.Provider>
   );
