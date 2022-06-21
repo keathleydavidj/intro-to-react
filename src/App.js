e03e9bbb9a26133f46b6416b5434a647d3b31457
David Keathley <keathleydavidj@gmail.com>
2022-06-21 12:29:26 -0700

    Restructure app for server side rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { StrictMode, useState } from "react";
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams.js";
 import Details from "./Details.js";
 import ThemeContext from "./ThemeContext.js";
@@ -11,18 +10,16 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <StrictMode>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
       </StrictMode>
     </ThemeContext.Provider>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
